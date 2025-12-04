df8465530848f0458d198d0a481db4bc7a888330
Elizabeth Frazier <elizabeth@egfrazier.com>
2025-12-03 22:56:15 -0800

    chore: Drop Python 3.7 and 3.8 support

--- setup.py
+++ setup.py
@@ -8,8 +8,6 @@ classifiers = [
     "Intended Audience :: System Administrators",
     "License :: OSI Approved :: Apache Software License",
     "Programming Language :: Python",
-    "Programming Language :: Python :: 3.7",
-    "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Topic :: Software Development",
     "Topic :: Software Development :: Documentation",
@@ -50,7 +48,7 @@ setup(
     package_dir={'': 'src'},
     package_data={'': ['buildout.cfg']},
     include_package_data=True,
-    python_requires=">=3.7",
+    python_requires=">=3.9",
     install_requires=[
         'blockdiag >= 3.0.0',
     ],
